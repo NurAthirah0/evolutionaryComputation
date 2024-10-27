a926a5b2653bf20eaf55502a35c6498cd5e63591
Nur Athirah <94262021+NurAthirah0@users.noreply.github.com>
2024-10-27 21:42:51 +0800

    Tutorial1.py

--- Tutorial1.py
+++ Tutorial1.py
@@ -4,3 +4,15 @@ st.set_page_config(
 )
 
 st.header("Genetic Algorithm", divider="gray")
+
+import random
+
+#POP_SIZE: Number of Chromosomes in our list.
+POP_SIZE = 500
+
+#TARGET: Our goal.
+TARGET = st.text_input("Enter Your Name")
+
+#GENES: Options from which our population would be created.
+GENES = ' abcdefghijklmnopqrstuvwxyz'
+
