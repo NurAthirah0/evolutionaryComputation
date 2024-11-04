1db2de84d3e5efe8d3f1785c49dad3cef840265a
Nur Athirah <94262021+NurAthirah0@users.noreply.github.com>
2024-11-05 00:53:27 +0800

    Update newTravellingSalesmanProblem.py

--- pages/newTravellingSalesmanProblem.py
+++ pages/newTravellingSalesmanProblem.py
@@ -12,26 +12,47 @@ cities_names = []
 x = []
 y = []
 
-for i in range(n_cities):
-    city_name = st.text_input(f"Enter the name of city {i+1}:", value=f"City{i+1}")
-    city_x = st.number_input(f"Enter the x-coordinate for {city_name}:", value=random.uniform(-10, 10))
-    city_y = st.number_input(f"Enter the y-coordinate for {city_name}:", value=random.uniform(-10, 10))
-    cities_names.append(city_name)
-    x.append(city_x)
-    y.append(city_y)
-
 city_coords = dict(zip(cities_names, zip(x, y)))
+n_population = 250
+crossover_per = 0.8
+mutation_per = 0.2
+n_generations = 200
 
-# Plotting the cities on the map
-fig, ax = plt.subplots()
+# Pastel Pallete
 colors = sns.color_palette("pastel", len(cities_names))
+
+# City Icons
+city_icons = {
+    "Gliwice": "♕",
+    "Cairo": "♖",
+    "Rome": "♗",
+    "Krakow": "♘",
+    "Paris": "♙",
+    "Alexandria": "♔",
+    "Berlin": "♚",
+    "Tokyo": "♛",
+    "Rio": "♜",
+    "Budapest": "♝"
+}
+
+fig, ax = plt.subplots()
+
+ax.grid(False)  # Grid
 
 for i, (city, (city_x, city_y)) in enumerate(city_coords.items()):
     color = colors[i]
+    icon = city_icons[city]
     ax.scatter(city_x, city_y, c=[color], s=1200, zorder=2)
-    ax.annotate(city, (city_x, city_y), fontsize=12, ha='center', va='center')
+    ax.annotate(icon, (city_x, city_y), fontsize=40, ha='center', va='center', zorder=3)
+    ax.annotate(city, (city_x, city_y), fontsize=12, ha='center', va='bottom', xytext=(0, -30),
+                textcoords='offset points')
 
-fig.set_size_inches(10, 8)
+    # Connect cities with opaque lines
+    for j, (other_city, (other_x, other_y)) in enumerate(city_coords.items()):
+        if i != j:
+            ax.plot([city_x, other_x], [city_y, other_y], color='gray', linestyle='-', linewidth=1, alpha=0.1)
+
+fig.set_size_inches(16, 12)
 st.pyplot(fig)
 
 #population
