f5e9eb7ea1a851d4b22d323ca8c47e50aa00eed7
Nur Athirah <94262021+NurAthirah0@users.noreply.github.com>
2024-11-05 01:53:26 +0800

    Update newTravellingSalesmanProblem.py

--- pages/newTravellingSalesmanProblem.py
+++ pages/newTravellingSalesmanProblem.py
@@ -84,6 +84,8 @@ for i, (city, (city_x, city_y)) in enumerate(city_coords.items()):
 fig.set_size_inches(16, 12)
 st.pyplot(fig)
 
+st.button("Submit")
+
 #population
 def initial_population(cities_list, n_population = 250):
 
