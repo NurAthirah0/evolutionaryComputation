c33a2b31cdce305383b76ba4107444a1d5ca3e84
Nur Athirah <94262021+NurAthirah0@users.noreply.github.com>
2024-12-10 06:12:53 +0800

    Update assignment.py

--- pages/assignment.py
+++ pages/assignment.py
@@ -32,10 +32,6 @@ file_path = 'pages/new_program_ratings.csv'
 # Get the data in the required format
 program_ratings_dict = read_csv_to_dict(file_path)
 
-# Print the result (you can also return or process it further)
-for program, ratings in program_ratings_dict.items():
-    st.write(f"'{program}': {ratings},")
-
 
 import random
 
