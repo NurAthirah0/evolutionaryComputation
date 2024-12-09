c04d0fb1f41b8cd8d7ee1d6958270ffd8e647be5
Nur Athirah <94262021+NurAthirah0@users.noreply.github.com>
2024-12-10 05:23:13 +0800

    Update assignment.py

--- pages/assignment.py
+++ pages/assignment.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-CO_R = st.number_input("Crossover Rate (range from 0.0 to 0.95)", min_value = 0.0, max_value = 0.05, 
+CO_R = st.number_input("Crossover Rate (range from 0.0 to 0.95)", min_value = 0.0, max_value = 0.95, 
                        value = 0.2, step= 0.01)
 
 MUT_R = st.number_input("Mutation Rate (range from 0.01 to 0.05)", min_value = 0.01, max_value = 0.05,
