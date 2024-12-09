ddc9280e482cbc9e8f6aa4c424518bc268d08297
Nur Athirah <94262021+NurAthirah0@users.noreply.github.com>
2024-12-10 04:50:56 +0800

    Update assignment.py

--- pages/assignment.py
+++ pages/assignment.py
@@ -1,5 +1,4 @@
 import streamlit as st
 
-with col1:
-  input_crossover = st.text_input("Crossover Rate", "range from 0 to 0.95", label_visibility = st.session_state.visibility, disabled = st.session_state.disabled, placeholder = st.session_state.placeholder,)
+input_crossover = st.text_input("Crossover Rate", "range from 0 to 0.95", label_visibility = st.session_state.visibility, disabled = st.session_state.disabled, placeholder = st.session_state.placeholder,)
 input_mutation = st.text_input("Mutation Rate", "0.2")
