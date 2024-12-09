bb8ca289d943350c794732ff3c89888f6c5fc93c
Nur Athirah <94262021+NurAthirah0@users.noreply.github.com>
2024-12-10 06:30:42 +0800

    Update assignment.py

--- pages/assignment.py
+++ pages/assignment.py
@@ -1,10 +1,14 @@
 import streamlit as st
 
+st.title("TV Program Schedule (assignment)")
+
 CO_R = st.number_input("Crossover Rate (range from 0.0 to 0.95)", min_value = 0.0, max_value = 0.95, 
                        value = 0.8, step= 0.01)
 
 MUT_R = st.number_input("Mutation Rate (range from 0.01 to 0.05)", min_value = 0.01, max_value = 0.05,
                         value = 0.02, step = 0.01)
+
+st.button("Generate")
 
 import csv
 
