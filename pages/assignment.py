528122c12bf3fbc2551d0989ba983722b7c3c6fd
Nur Athirah <94262021+NurAthirah0@users.noreply.github.com>
2024-12-10 06:18:42 +0800

    Update assignment.py

--- pages/assignment.py
+++ pages/assignment.py
@@ -6,8 +6,6 @@ CO_R = st.number_input("Crossover Rate (range from 0.0 to 0.95)", min_value = 0.
 MUT_R = st.number_input("Mutation Rate (range from 0.01 to 0.05)", min_value = 0.01, max_value = 0.05,
                         value = 0.02, step = 0.01)
 
-st.button("Enter")
-
 import csv
 
 # Function to read the CSV file and convert it to the desired format
